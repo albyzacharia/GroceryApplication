1ba51eec1385a51b12aff2ee816b58e1f243de77
Dell <Dell@DELL-PC.lan>
2024-10-28 22:58:27 +0530

    Second Commit

---
+++ GroceryApplication/src/main/java/elementRepository/LoginPage.java
@@ -0,0 +1,51 @@
+package elementRepository;
+
+import java.io.IOException;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import utilities.ExcellUtilities;
+
+public class LoginPage {
+	WebDriver driver;
+	ExcellUtilities eu=new ExcellUtilities();
+	public LoginPage(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);// to initialize page factory-static method of page factory
+	}
+	@FindBy(xpath = "//input[@name='username']")
+	WebElement userNameField;
+	@FindBy(xpath = "//input[@name='password']")
+	WebElement password;
+	@FindBy(xpath="//button[text()='Sign In']")
+	WebElement signinButton;
+	@FindBy(xpath="//div[@class='alert alert-danger alert-dismissible']")
+	WebElement alertMessage;
+
+
+
+public void sendLoginDetails(String userName,String passWord)
+{
+	userNameField.sendKeys(userName);
+	password.sendKeys(passWord);
+	signinButton.click();
+}
+public String getWarningMessageText()
+{
+return 	alertMessage.getText();
+}
+public HomePage loginToApplication() throws IOException
+{
+		
+	userNameField.sendKeys(ExcellUtilities.readStringData(0, 0));	
+	password.sendKeys(ExcellUtilities.readStringData(0, 1));
+	signinButton.click();
+	return new HomePage(driver);
+	
+}
+
+}
+
---
+++ GroceryApplication/src/test/java/testCase/SubCategoryTest.java
@@ -0,0 +1,85 @@
+package testCase;
+
+import java.io.IOException;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import elementRepository.HomePage;
+import elementRepository.LoginPage;
+import elementRepository.SubCategory;
+
+public class SubCategoryTest extends BaseClass {
+	LoginPage lp;
+	HomePage hp;
+	SubCategory sc;
+
+	@Test(enabled=false)
+	public void verifyNewSubcategory() throws IOException {
+		lp = new LoginPage(driver);
+		//hp = new HomePage(driver);
+		//sc = new SubCategory(driver);
+		//lp.sendLoginDetails("admin", "admin");
+		hp=lp.loginToApplication();
+		sc=hp.clickOnSubcategoryButton();
+		sc.addNewSubcategory();
+		String subCatName = sc.enterSubCategoryName();
+		sc.saveSubCategory();
+		sc.isAlertDisplayed();
+		String actual = sc.gettextalert();
+		System.out.println(actual);
+		String expected = "�\n" + "Alert!\n" + "Sub Category Created Successfully";
+		Assert.assertEquals(actual, expected, "SubCategory Creation is not as expected");// hard assertion
+		hp.clickOnSubcategoryButton();
+		String actualSubCategoryName = sc.readSubcategoryTableElement(1, 1);
+		String expectedSubcategoryName = subCatName;
+		Assert.assertEquals(actualSubCategoryName, expectedSubcategoryName, "SubCategory not added to the table");
+
+	}
+
+	@Test(enabled=false)
+	public void verifyEditSubcategory() throws IOException {
+		lp = new LoginPage(driver);
+		//hp = new HomePage(driver);
+		//sc = new SubCategory(driver);
+		//lp.sendLoginDetails("admin", "admin");
+		hp=lp.loginToApplication();
+		sc=hp.clickOnSubcategoryButton();
+		String editcatName = sc.categoryNametoEditCat();
+		String editsubcatname = sc.subCategoryNametoEdit();
+		sc.clickSubCategoryEditButton();
+		sc.clearenterSubCategoryName();
+		String newSubcat = sc.enterSubCategoryName();
+		sc.updateEditSubCategory();
+		boolean actualAlertmessage = sc.updateAlertDisplated();
+		System.out.println(actualAlertmessage);
+		Assert.assertEquals(actualAlertmessage, true);
+		String actualnewSubCategoryName = sc.readSubcategoryTableElement(1, 1);
+		String expectednewSubcategoryName = newSubcat;		
+		Assert.assertEquals(actualnewSubCategoryName, expectednewSubcategoryName,
+				"SubCategory not edited to the table");
+		sc.confirmSubCatSearch(editsubcatname, editcatName);
+		boolean searchResult=sc.resultNotFouncSearchMsg();
+		Assert.assertEquals(searchResult, true);
+
+	}
+	@Test
+	public void verifyDeleteSubcategory() throws IOException {
+		lp = new LoginPage(driver);
+		hp = new HomePage(driver);
+		sc = new SubCategory(driver);
+		//lp.sendLoginDetails("admin", "admin");
+		lp.loginToApplication();
+		hp.clickOnSubcategoryButton();
+		String editcatName = sc.categoryNametoEditCat();
+		String editsubcatname = sc.subCategoryNametoEdit();
+		boolean msgdelete=sc.deleteSubCategory();
+		Assert.assertEquals(msgdelete, true);
+		sc.confirmSubCatSearch(editsubcatname, editcatName);
+		boolean searchResult=sc.resultNotFouncSearchMsg();
+		Assert.assertEquals(searchResult, true);
+		
+		
+
+}
+}
